8104df23a2d42f7c753a785053ef25ebb05d26fe
odanielson <olov.danielson@gmail.com>
2013-12-15 11:32:23 +0100

    first commit with a few basic functions.

---
+++ vector_2d.js
@@ -0,0 +1,31 @@
+// Copyright 2013, odanielson@github.com
+// MIT-license
+
+var Norm = function(v) {
+    return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+}
+
+var Normalize = function(v) {
+    var norm = Norm(v);
+    return [v[0] / norm, v[1] / norm];
+}
+
+var Subtract = function(a, b) {
+    return [a[0] - b[0], a[1] - b[1]];
+}
+
+var Add = function(a, b) {
+    return [a[0] + b[0], a[1] + b[1]];
+}
+
+var Multiply = function(v, k) {
+    return [v[0] * k, v[1] * k];
+}
+
+var ComplexMultiply = function(v1, v2) {
+    return [ (v1[0] * v2[0]) - (v1[1] * v2[1]), (v1[0] * v2[1]) + (v2[0] * v1[1]) ];
+}
+
+var RotateLeft = function(v) {
+    return ComplexMultiply(v, [0,-1]);
+}
